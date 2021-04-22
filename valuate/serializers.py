57010dc66c7dfd8b11e12266fe8d17c721db3e6a
Chanuka-Gishen <76777641+Chanuka-Gishen@users.noreply.github.com>
2021-04-22 23:06:48 +0530

    Added API

---
+++ valuate/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from valuate.models import UserValuationDetails
+
+class UserInputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserValuationDetails
+        fields = (
+            'size',
+            'land_type',
+            'distance_to_town',
+            'calculate_year',
+            'lane'
+        )
+        
